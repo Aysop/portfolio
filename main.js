ed0785abe1fa646a0ec06579378ed0267fc0fa6f
Cotton3y3 <47838762+Cotton3y3@users.noreply.github.com>
2019-03-31 16:59:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 // Set button to click.
-var button = document.getElementById('menu-toggle');
+var button = document.getElementById("menu-toggle");
 
 // Click the button.
 button.onclick = function () {
-
+    use strict
     // Toggle class "opened". Set also aria-expanded to true or false.
-    if (-1 !== button.className.indexOf('opened')) {
-        button.className = button.className.replace(' opened', '');
-        button.setAttribute('aria-expanded', 'false');
+    if (-1 !== button.className.indexOf("opened")) {
+        button.className = button.className.replace(" opened", "");
+        button.setAttribute("aria-expanded", "false");
     } else {
-        button.className += ' opened';
-        button.setAttribute('aria-expanded', 'true');
+        button.className += " opened";
+        button.setAttribute("aria-expanded", "true");
     }
+}
 
-};
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
