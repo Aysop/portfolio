3db71137373d6ab544522de0c3fef645bf49bf18
Cotton3y3 <47838762+Cotton3y3@users.noreply.github.com>
2019-04-26 15:07:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ button.onmouseover = function () {
         button.setAttribute("aria-expanded", "true");
     }
 }
+
+// caesar cipher
+$('input').keyup(function() {
+  var k = Number($('#key').val());
+  if (!(k >=0 && k < 26)) {
+    $('#output').html('Key should be between 0 and 25');
+    return;
+  }
+  var secret = $('#text').val().toLowerCase();
+  var encoded = '';
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (var j = 0; j < secret.length; j += 1) {
+    var nextchar = secret.charAt(j);
+    var index = alphabet.indexOf(nextchar);
+    if (index >= 0) {
+      var shifted = (index + k) % 26;
+      encoded += alphabet.charAt(shifted);
+    } else {
+      encoded += nextchar;
+    }
+  }
+  $('#output').html(encoded);
+});
