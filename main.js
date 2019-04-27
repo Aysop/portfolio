37b6e40d95837ba5bf8baff80ab16dc4044adbb4
Cotton3y3 <47838762+Cotton3y3@users.noreply.github.com>
2019-04-27 11:11:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,24 +15,43 @@ button.onmouseover = function () {
 }
 
 // caesar cipher
-$('input').keyup(function() {
-  var k = Number($('#key').val());
-  if (!(k >=0 && k < 26)) {
-    $('#output').html('Key should be between 0 and 25');
-    return;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var fullAlphabet = alphabet + alphabet + alphabet;
+
+function runCipher(){
+  var cipherText = $('#cypher').val();
+  var cipherOffset = $('#offset').val();
+  cipherOffset = (cipherOffset % alphabet.length);
+  var cipherFinish = '';
+
+  for(i=0; i<cipherText.length; i++){
+     var letter = cipherText[i];
+     var upper = (letter == letter.toUpperCase());
+     letter = letter.toLowerCase();
+    
+     var index = alphabet.indexOf(letter);
+     if(index == -1){
+       cipherFinish += letter;
+     } else {
+       index = ((index + cipherOffset) + alphabet.length);
+       var nextLetter = fullAlphabet[index];
+       if(upper) nextLetter = nextLetter.toUpperCase();
+       cipherFinish += nextLetter;
+     }
   }
-  var secret = $('#text').val().toLowerCase();
-  var encoded = '';
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  for (var j = 0; j < secret.length; j += 1) {
-    var nextchar = secret.charAt(j);
-    var index = alphabet.indexOf(nextchar);
-    if (index >= 0) {
-      var shifted = (index + k) % 26;
-      encoded += alphabet.charAt(shifted);
-    } else {
-      encoded += nextchar;
-    }
-  }
-  $('#output').html(encoded);
+    
+  $('#finish').val(cipherFinish);
+}
+
+$(document).ready(function() {
+  $('#cypher').keypress(function(){
+    setTimeout(function(){ runCipher(); },20);
+  });
+  $('#cypher').blur(function(){
+    runCipher();
+  });
+  $('#offset').change(function(){
+    setTimeout(runCipher(),20);
+  });
+  
 });
